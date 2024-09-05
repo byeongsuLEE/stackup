747b964d1de20f971f98cbf518fab33b558a9209
전지훈 <ggh2111@naver.com>
2024-09-05 11:44:45 +0900

    style(footer): 푸터 css 수정

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -22,7 +22,9 @@ const Footer: React.FC = () => {
       location.pathname.match("/work") ||
       location.pathname.match("/project") ||
       location.pathname.match("/account") ||
-      location.pathname.match("/career")
+      location.pathname.match("/career") ||
+      location.pathname.match("/mypage") ||
+      location.pathname.match("/login")
     ) {
       setHiddenFooter(true);
     } else {
@@ -73,8 +75,8 @@ const Footer: React.FC = () => {
   const menuItems: MenuItem[] = [
     { text: "일감", path: "/work" },
     { text: "프로젝트", path: "/project" },
-    { text: "계좌관리", path: "/account" },
     { text: "경력관리", path: "/career" },
+    { text: "계좌관리", path: "/account" },
   ];
 
   return (
@@ -85,19 +87,22 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-4 py-4 flex justify-between items-start">
         {/* 로고 및 아이콘 섹션 */}
         <div className="flex flex-col w-1/3 gap-4">
-          <img src="/logos/Logo.png" alt="footer_logo" className="w-24" />
-          <div className="flex gap-2 text-mainTxt flex-wrap">
+          <div className="flex gap-2">
+            <img src="/logos/Logo.png" alt="footer_logo" className="w-8" />
+            <p className="font-semibold text-subGreen1">STACK UP</p>
+          </div>
+          <div className="flex gap-2 text-black flex-wrap">
             {iconsTab.map(({ icon, url, name }, index) => (
               <div key={index} className="flex flex-col items-center">
                 <a
                   href={url}
                   target="_blank"
                   rel="noreferrer"
-                  className="text-lg bg-gray-200 p-1.5 rounded-full hover:bg-mainBlue hover:text-white transition-all duration-300 cursor-pointer mb-1"
+                  className="text-lg bg-gray-200 p-1.5 rounded-full hover:bg-mainGreen hover:text-white transition-all duration-300 cursor-pointer mb-1"
                 >
                   {icon}
                 </a>
-                <span className="text-xs text-mainTxt">{name}</span>
+                <span className="text-xs text-boldTxt">{name}</span>
               </div>
             ))}
           </div>
@@ -108,12 +113,12 @@ const Footer: React.FC = () => {
         <div className="flex gap-10 w-2/3 justify-end">
           <div className="flex flex-col gap-2">
             <p className="text-lg font-bold">Menu</p>
-            <span className="block w-16 h-0.5 bg-mainBlue"></span>
+            <span className="block w-16 h-0.5 bg-subGreen1"></span>
             {menuItems.map(({ text, path }, index) => (
               <Link
                 key={index}
                 to={path}
-                className="text-sm text-mainTxt hover:text-subBlue cursor-pointer"
+                className="text-sm font-medium text-boldTxt hover:text-subGreen1 cursor-pointer"
               >
                 {text}
               </Link>
@@ -122,20 +127,20 @@ const Footer: React.FC = () => {
 
           <div className="flex flex-col gap-2">
             <p className="text-lg font-bold">Team</p>
-            <span className="block w-40 h-0.5 bg-mainBlue"></span>
+            <span className="block w-40 h-0.5 bg-subGreen1"></span>
             <div className="flex gap-6">
               <div className="flex flex-col">
-                <p className="text-sm text-mainTxt font-bold">Front-end:</p>
+                <p className="text-sm text-boldTxt font-bold">Front-end:</p>
                 {["이채연", "이호영", "전지훈"].map((name, index) => (
-                  <p key={index} className="text-xs text-mainTxt">
+                  <p key={index} className="text-xs text-boldTxt">
                     {name}
                   </p>
                 ))}
               </div>
               <div className="flex flex-col">
-                <p className="text-sm text-mainTxt font-bold">Back-end:</p>
+                <p className="text-sm text-boldTxt font-bold">Back-end:</p>
                 {["김연지", "이병수", "최승호"].map((name, index) => (
-                  <p key={index} className="text-xs text-mainTxt">
+                  <p key={index} className="text-xs text-boldTxt">
                     {name}
                   </p>
                 ))}

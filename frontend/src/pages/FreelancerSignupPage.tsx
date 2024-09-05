b6471c9ef0f06e212b4fa0a255a09e4f464af19f
이호영 <hoo4053@naver.com>
2024-09-05 22:46:26 +0900

    feat(signup) todo 프리랜서 회원가입

---
+++ frontend/src/pages/FreelancerSignupPage.tsx
@@ -0,0 +1,11 @@
+import { DiGhost } from "react-icons/di"
+import FreelancerInfo from "../components/SignupPage/FreelancerInformation";
+
+const FreelancerSignup = () => {
+  return ( 
+    <div className="flex h-screen justify-center my-20">
+    <FreelancerInfo />
+    </div>
+  )
+}
+export default FreelancerSignup;

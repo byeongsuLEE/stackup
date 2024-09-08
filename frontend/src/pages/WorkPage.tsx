83e29a3f8a2b1516f72f6acb1f6a160bd35aaef5
이호영 <hoo4053@naver.com>
2024-09-08 21:45:50 +0900

    feat(work) todo 일감 목록 페이지

--- frontend/src/pages/WorkPage.tsx
+++ frontend/src/pages/WorkPage.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import DoneButton from '../components/common/DoneButton';
+import ReccommnendWork from '../components/WorkPage/RecommendWork';
 
 const Work = () => {
   const navigate = useNavigate();
@@ -7,11 +8,13 @@ const Work = () => {
     navigate('/work/post');
   }
   return (
-    <div className='flex justify-center'>
+    <div className='flex flex-col'>
       {/* 클라이언트 일감 페이지 */}
-      <div onClick={toPostWork} className='my-10'>
+      <div onClick={toPostWork} className='my-10 flex justify-center'>
       <DoneButton width={200} height={32} title="프로젝트 등록하기" />
       </div>
+      {/* 프리랜서 추천 일감 */}
+      <ReccommnendWork/>
     </div>
   )
 }

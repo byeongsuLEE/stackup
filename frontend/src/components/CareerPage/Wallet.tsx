cc72d9d89b849e342c718c5aceb14b47e17dcba8
이호영 <hoo4053@naver.com>
2024-09-23 13:22:42 +0900

    feat(nft): todo 트랜잭션

--- frontend/src/components/CareerPage/Wallet.tsx
+++ frontend/src/components/CareerPage/Wallet.tsx
@@ -2,10 +2,8 @@ import { useEffect, useState } from "react";
 import { useWeb3 } from "../../hooks/useWeb3";
 
 const Wallet = () => {
-  const { account, web3, ssfBalance } = useWeb3();
+  const { account, ssfBalance } = useWeb3();
   const [isLogin, setIsLogin] = useState<boolean>();
-  const [balance, setBalance] = useState<number>();
-
 
   useEffect(() => {
     if (account === null) {
@@ -17,25 +15,25 @@ const Wallet = () => {
 
   return (
     <>
-    {isLogin?(
-      <div className="bg-bgGreen w-auto h-[120px] flex flex-col border border-mainGreen rounded-lg p-5">
-        <span>나의 지갑 정보</span>
-        <span>계정 : {account}</span>
-        <span>잔액 : {ssfBalance} SSF</span>
-      </div>
-    ):(
-      <div className="text-center mb-5">
-        <span className="mr-1">
-        NFT경력관리를 위한 지갑을 사용을 위해
-        </span>
-        <a className=" text-blue-500 underline" href="https://metamask.io/ko/">
-        MetaMask
-        </a>
-        <span>
-        를 설치해주세요.
-        </span>
-      </div>
-    )}
+      {isLogin ? (
+        <div className="bg-bgGreen w-auto h-[120px] flex flex-col border border-mainGreen rounded-lg p-5">
+          <span className="font-bold text-subGreen1 mb-2">나의 지갑 정보</span>
+          <span>계정 : {account}</span>
+          <span>잔액 : {ssfBalance} SSF</span>
+        </div>
+      ) : (
+        <div className="text-center mb-5">
+          <span className="mr-1">
+            NFT경력관리를 위한 지갑을 사용을 위해
+          </span>
+          <a className=" text-blue-500 underline" href="https://metamask.io/ko/">
+            MetaMask
+          </a>
+          <span>
+            를 설치해주세요.
+          </span>
+        </div>
+      )}
     </>
   )
 }
--- frontend/src/hooks/useWeb3.ts
+++ frontend/src/hooks/useWeb3.ts
@@ -1,95 +1,3 @@
-// import { MetaMaskInpageProvider } from '@metamask/providers';
-// import { useEffect, useState } from 'react';
-// import Web3 from 'web3';
-
-// export const useWeb3 = () => {
-//   const [account, setAccount] = useState<string | null>(null);
-//   const [isRequestPending, setIsRequestPending] = useState(false);
-//   const [web3, setWeb3] = useState<Web3 | null>(null);
-
-//   const getCurChainId = async (): Promise<string | null> => {
-//     const eth = window.ethereum as MetaMaskInpageProvider;
-//     try {
-//       const curChainId = await eth.request({
-//         method: 'eth_chainId',
-//       });
-//       return curChainId as string;
-//     } catch (error) {
-//       console.error('Error fetching chain ID:', error);
-//       return null;
-//     }
-//   };
-
-//   const addAndConnNetwork = async (chainId: string) => {
-//     const eth = window.ethereum as MetaMaskInpageProvider;
-//     const network = {
-//       chainId,
-//       chainName: 'SSAFY',
-//       rpcUrls: ['https://rpc.ssafy-blockchain.com'],
-//       nativeCurrency: {
-//         name: 'SSF Token',
-//         symbol: 'ETH',
-//         decimals: 18,
-//       },
-//     };
-
-//     try {
-//       if (!isRequestPending) {
-//         setIsRequestPending(true);
-//         const response = await eth.request({
-//           method: 'wallet_addEthereumChain',
-//           params: [network],
-//         });
-//         console.log(response)
-//         console.log('Network added and connected successfully.');
-//       } else {
-//         console.log('Network addition request is already in progress.');
-//       }
-//     } catch (error) {
-//       console.error('Error adding and connecting network:', error);
-//     } finally {
-//       setIsRequestPending(false);
-//     }
-//   };
-
-//   const getAccount = async () => {
-//     const eth = window.ethereum as MetaMaskInpageProvider;
-//     try {
-//       const accounts = await eth.request({
-//         method: 'eth_requestAccounts',
-//       });
-//       return accounts;
-//     } catch (error) {
-//       console.error('Error fetching account:', error);
-//       return [];
-//     }
-//   };
-
-//   useEffect(() => {
-//     const connectToNetwork = async () => {
-//       if (window.ethereum) {
-//         const curChainId = await getCurChainId();
-//         const targetChainId = '0x79f5';
-
-//         if (curChainId !== targetChainId) {
-//           await addAndConnNetwork(targetChainId);
-//         } else {
-//           console.log('Already connected to SSAFY network.');
-//         }
-
-//         const [userAccount] = (await getAccount()) as string[];
-//         setAccount(userAccount);
-
-//         const web3Instance = new Web3(window.ethereum);
-//         setWeb3(web3Instance);
-//       }
-//     };
-
-//     connectToNetwork();
-//   }, []);
-
-//   return { account, web3, isRequestPending };
-// };
 import { MetaMaskInpageProvider } from '@metamask/providers';
 import { useEffect, useState } from 'react';
 import Web3 from 'web3';
@@ -181,7 +89,7 @@ export const useWeb3 = () => {
     if (!web3Instance || !userAccount) return;
 
     const ssfContract = new web3Instance.eth.Contract(ERC20_ABI as any, SSF_TOKEN_CONTRACT_ADDRESS);
-    
+
     try {
       const balance = await ssfContract.methods.balanceOf(userAccount).call();
       const decimals = await ssfContract.methods.decimals().call();

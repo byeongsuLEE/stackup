7711f63777979d2d5152d029dd4f31ba442eb75d
이호영 <hoo4053@naver.com>
2024-09-29 23:10:41 +0900

    faet(nft): nft 페이지에 렌더링 완료

--- blockchain/NFT/contracts/MyNFT.sol
+++ blockchain/NFT/contracts/MyNFT.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract MyNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
     uint256 public nextTokenId;
-    address public admin;
 
     // NFT 민팅 시 이벤트
     event Minted(address recipient, uint256 tokenId, string tokenURI);
@@ -34,8 +34,23 @@ contract MyNFT is ERC721URIStorage, Ownable {
     }
 
     // 토큰 URI를 반환하는 함수
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         require(tokenExists(tokenId), "Token does not exist.");
         return super.tokenURI(tokenId);
+    }
+
+    // ERC721Enumerable과 ERC721URIStorage의 supportsInterface를 오버라이드
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // 두 개의 부모 계약에서 정의된 _beforeTokenTransfer를 오버라이드합니다.
+    function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal virtual override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
+    // 두 개의 부모 계약에서 정의된 _burn을 오버라이드합니다.
+    function _burn(uint256 tokenId) internal virtual override(ERC721URIStorage, ERC721) {
+        super._burn(tokenId);
     }
 }
--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,70 +1,86 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import DoneButton from "../components/common/DoneButton";
 import { CallTest } from "../hooks/Test";
 import { generateImage, nftInfoProp } from "../hooks/MakeImage";
 import { pinata, uploadMetadataToPinata } from "../apis/NftApi";
+import NFTDisplay from "../components/NFTPage/NFTDisplay";
+
+// window.ethereum 타입 확장
+declare global {
+  interface Window {
+    ethereum?: any; // 혹은 정확한 타입을 명시하고 싶다면 Web3Provider 타입을 사용할 수 있습니다.
+  }
+}
 
 const Test = () => {
-  const { Minting } = CallTest()
+  const { Minting } = CallTest();
   const ethereum = window.ethereum;
   const [addr, setAddr] = useState("");
-  //const web3 = new Web3(Web3.givenProvider);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
   //== 데이터 임시 값 ==//
   const defaultNftInfo: nftInfoProp = {
-  projectName: "Default Project",
-  companyName: "Default Company",
-  period: "2024.01.01 ~ 2024.12.31",
-  name: "Default Name"
+    projectName: "Default Project",
+    companyName: "Default Company",
+    period: "2024.01.01 ~ 2024.12.31",
+    name: "Default Name",
   };
 
-  const handleGetAcount = async e => {
-    const accounts = await window.ethereum.enable();
-    const account = accounts[0];
-    setAddr(account);
+  // 지갑 연결 함수
+  const handleGetAccount = async () => {
+    try {
+      const accounts = await window.ethereum.enable();
+      const account = accounts[0];
+      setAddr(account);
+    } catch (error) {
+      console.error("지갑 연결 오류:", error);
+    }
   };
 
-  if (ethereum) {
-    ethereum.on("accountsChanged", function (accounts) {
-      setAddr(accounts[0]);
-    });
-  }
+  // accountsChanged 이벤트 처리
+  useEffect(() => {
+    if (ethereum) {
+      ethereum.on("accountsChanged", function (accounts: string[]) {
+        setAddr(accounts[0]);
+      });
+    }
+  }, [ethereum]);
 
   // 버튼 클릭 시 호출될 핸들러 함수
   const handleMintNFT = async () => {
-
-    const data = await generateImage(canvasRef);
-    const hash = await pinata(data);
-    console.log(hash)
-    const cid = await uploadMetadataToPinata(hash, defaultNftInfo);
-  
     try {
+      const data = await generateImage(canvasRef);
+      const hash = await pinata(data);
+      console.log(hash);
+      const cid = await uploadMetadataToPinata(hash, defaultNftInfo);
+
       // NFT 발행
-      await Minting(cid)
-      alert("NFT 발행 호출");
+      await Minting(cid);
+      alert("NFT 발행 호출 성공");
     } catch (error) {
       console.error("NFT 발행 오류:", error);
       alert("NFT 발행 실패");
     }
   };
 
-
   return (
     <div>
       {ethereum && (
-        <div onClick={handleGetAcount} >
+        <div onClick={handleGetAccount}>
           <DoneButton height={30} width={200} title="Connect Wallet" />
         </div>
       )}
       {ethereum && <p>Your Wallet address: {addr}</p>}
 
-       {/* 캔버스 요소 추가 */}
-       <canvas ref={canvasRef} style={{ border: "1px solid black" }}></canvas>
+      {/* 캔버스 요소 추가 */}
+      <canvas ref={canvasRef} style={{ border: "1px solid black" }}></canvas>
 
       <div onClick={handleMintNFT}>
         <DoneButton height={30} width={200} title="Contract 배포" />
       </div>
+      <NFTDisplay/>
     </div>
-  )
-}
+  );
+};
+
 export default Test;

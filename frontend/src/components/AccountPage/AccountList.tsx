0230e7a924cbd3729b698e7c501696e90682a9c7
이호영 <hoo4053@naver.com>
2024-10-04 20:38:50 +0900

    feat(account):계좌 페이지 css 수정 완료

--- frontend/src/components/AccountPage/AccountList.tsx
+++ frontend/src/components/AccountPage/AccountList.tsx
@@ -6,21 +6,24 @@ import MainAccount from "./MainAccount";
 const AccountList = ({ accountList, mainAccount }: { accountList: accountInfo[]; mainAccount: string }) => {
 
   return (
-    <div className="flex flex-wrap">
-      <div className="flex justify-center">
-        {accountList.map((account: accountInfo) => (
-          <Link to={`/account/detail/${account.accountId}`} key={account.accountId}>
-            {account.accountNum === mainAccount ? (
-              <MainAccount {...account}/>
-            ) : (
-              <Account {...account}/>
-            )}
+    <div className="flex flex-wrap justify-center">
+      {accountList.map((account: accountInfo) => (
+        account.accountNum === mainAccount ? (
+          // MainAccount는 전체 너비 차지하고 가운데 정렬
+          <Link to={`/account/detail/${account.accountId}`} key={account.accountId} className="w-full p-2">
+              <MainAccount {...account} />
           </Link>
-        ))}
-        
-      </div>
-
+        ) : (
+          // Account는 한 줄에 3개씩 배치되고 가운데 정렬
+          <Link to={`/account/detail/${account.accountId}`} key={account.accountId} className="w-full sm:w-1/2 md:w-1/3 p-2">
+            <div className="flex justify-center">
+              <Account {...account} />
+            </div>
+          </Link>
+        )
+      ))}
     </div>
-  )
+  );
 }
+
 export default AccountList;

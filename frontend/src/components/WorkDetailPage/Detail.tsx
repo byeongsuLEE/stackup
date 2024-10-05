4a122a07d0630e17f44f471254efc0c436d3453e
이채연 <dldus0621@naver.com>
2024-10-05 17:09:42 +0900

    fix(detail): 페이지 오류 수정

--- frontend/src/components/WorkDetailPage/Detail.tsx
+++ frontend/src/components/WorkDetailPage/Detail.tsx
@@ -101,22 +101,21 @@ const Detail = ({ project, clientId }: DetailProps) => {
           <span className="text-subTxt text-sm">{project?.client.businessName} _ 평점 {project?.client.totalScore}wja</span>
         </div>
         <div className="flex justify-end">
-          {window.sessionStorage.getItem("userType") === 'freelancer' ? (
-            <div onClick={projectApplyHandler}>
-              <DoneButton width={100} height={25} title="지원하기" />
+        {window.sessionStorage.getItem("userType") === "freelancer" ? (
+          <div onClick={projectApplyHandler}>
+            <DoneButton width={100} height={25} title="지원하기" />
+          </div>
+        ) : sessionClientId == clientId && (
+          <div className="flex">
+            <div onClick={toCandidate}>
+              <DoneButton width={100} height={25} title="지원자 관리" />
             </div>
-          ) : (
-            window.sessionStorage.getItem("clientId") == clientId ? (
-              <div className="flex">
-                <div onClick={toCandidate}>
-                  <DoneButton width={100} height={25} title="지원자 관리" />
-                </div>
-                <button className="bg-subGreen2 text-bgGreen font-bold text-sm px-3 rounded-lg ml-2">마감하기</button>
-              </div>
-            ) : (
-              <div></div>
-            )
-          )}
+            <Payment boardId={boardId} />
+            <button onClick={deleteProject} className="bg-subGreen2 text-bgGreen font-bold text-sm px-3 rounded-lg ml-2">
+              삭제하기
+            </button>
+          </div>
+        ) }
 
         </div>
 
@@ -140,71 +139,6 @@ const Detail = ({ project, clientId }: DetailProps) => {
             <span>기타 요구사항</span>
           </div>
           <div className="flex flex-col">
-            <div className="flex items-center">
-              <span>{project.deadline.toString()}</span>
-              <span className="text-xs ml-2 text-red-400">마감 {remainDay}일 전</span>
-            </div>
-            <span>{project.recruits} 명</span>
-            <span>{project.startDate.toString()}</span>
-            <span>{workType}</span>
-
-            {/* 수정필요 => 데이터 안옴*/}
-            <span>{languagesList.join(', ')}</span>
-            <span>{frameworksList.join(', ')}</span>
-            {/* <span>{frameworksString}</span>
-            <span>{languagesString}</span> */}
-            {/* 수정 필요 */}
-            <span>{project.requirements}</span>
-          </div>
-        </div>
-
-        <div className="bg-subTxt w-auto h-[1px] flex justify-center my-10"></div>
-
-        <div>
-          <div className="font-bold text-lg mb-2">업무 내용</div>
-          <br />
-          <span><pre>{project.description}</pre></span>
-        </div>
-
-      </div>
-      <div className="flex justify-end">
-        {window.sessionStorage.getItem("userType") === "freelancer" ? (
-          <div onClick={projectApplyHandler}>
-            <DoneButton width={100} height={25} title="지원하기" />
-          </div>
-        ) : sessionClientId == clientId && (
-          <div className="flex">
-            <div onClick={toCandidate}>
-              <DoneButton width={100} height={25} title="지원자 관리" />
-            </div>
-            <Payment boardId={boardId} />
-            <button onClick={deleteProject} className="bg-subGreen2 text-bgGreen font-bold text-sm px-3 rounded-lg ml-2">
-              삭제하기
-            </button>
-          </div>
-        ) }
-      </div>
-
-      <div className="bg-subTxt w-auto h-[1px] flex justify-center my-10"></div>
-
-      <div className="flex justify-center mb-10">
-        <InfoBox title="예상 금액" category="deposit" content={project.deposit} info={PriceIcon} />
-        <InfoBox title="예상 기간" category="period" content={project.period} info={PeriodIcon} />
-        <InfoBox title="지원자 수" category="applicants" content={project.applicants} info={CandidateIcon} />
-      </div>
-
-      <div className="flex ml-10">
-        <div className="flex flex-col mr-20 text-subTxt">
-          <span>모집 마감일</span>
-          <span>모집 인원</span>
-          <span>프로젝트 시작일</span>
-          <span>근무 형태</span>
-
-          <span>사용언어</span>
-          <span>프레임워크</span>
-          <span>기타 요구사항</span>
-        </div>
-        <div className="flex flex-col">
           <div className="flex items-center">
             <span>{project.deadline.toString()}</span>
             <span className="text-xs ml-2 text-red-400">마감 {remainDay}일 전</span>
@@ -217,16 +151,15 @@ const Detail = ({ project, clientId }: DetailProps) => {
           <span>{frameworksList.join(", ")}</span>
           <span>{project.requirements}</span>
         </div>
-      </div>
+        </div>
 
-      <div className="bg-subTxt w-auto h-[1px] flex justify-center my-10"></div>
+        <div className="bg-subTxt w-auto h-[1px] flex justify-center my-10"></div>
 
-      <div>
-        <div className="font-bold text-lg mb-2">업무 내용</div>
-        <br />
-        <span>
-          <pre>{project.description}</pre>
-        </span>
+        <div>
+          <div className="font-bold text-lg mb-2">업무 내용</div>
+          <br />
+          <span><pre>{project.description}</pre></span>
+        </div>
       </div>
     </>
   );

0ce1dd68e56c719c074fa9642f227942c315e76d
이병수 <evil55@naver.com>
2024-10-07 20:47:41 +0900

    feat : 없어진 user를 넣기

---
+++ backend/user/src/main/java/com/ssafy/stackup/domain/user/dto/response/UserInfoResponseDto.java
@@ -0,0 +1,24 @@
+package com.ssafy.stackup.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoResponseDto {
+    private Long id;
+    protected String name;
+    protected String email;
+    private String phone;
+    private String secondPassword;
+    private String accountKey;
+    private String mainAccount;
+    private Double totalScore;
+    private Integer reportedCount;
+    protected List<String> roles;
+}

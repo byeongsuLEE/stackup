c39f36858d12aaba8666c3c5e944558e83250fde
이병수 <evil55@naver.com>
2024-10-08 10:46:37 +0900

    feat : dev/be 업데이트

--- backend/user/src/main/java/com/ssafy/stackup/domain/user/service/UserServiceImpl.java
+++ backend/user/src/main/java/com/ssafy/stackup/domain/user/service/UserServiceImpl.java
@@ -34,7 +34,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
-import retrofit2.http.HEAD;
 
 import java.util.*;
 

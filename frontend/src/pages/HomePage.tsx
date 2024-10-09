0c1eeb8ee6621d48330404740e818d4a3d3e178f
이호영 <hoo4053@naver.com>
2024-10-10 00:24:42 +0900

    feat(todo 메인페이지

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -8,12 +8,12 @@ import "splitting/dist/splitting.css";
 import ContentSection from "../components/HomePage/ContentSection";
 import TextEffectSection from "../components/HomePage/TextEffectSection";
 import "../components/HomePage/styles.css";
-import RecommendIcon from "../icons/Recommend";
 import DoneButton from "../components/common/DoneButton";
-import WorkExample from "../icons/WorkExample";
-import EvaluationIcon from "../icons/EvaluationIcon";
-import DetectionIcon from "../icons/DetectionIcon";
 import ContractIcon from "../icons/ContractIcon";
+import DetectionIcon from "../icons/DetectionIcon";
+import EvaluationIcon from "../icons/EvaluationIcon";
+import RecommendIcon from "../icons/Recommend";
+import WorkExample from "../icons/WorkExample";
 // GSAP와 ScrollTrigger 플러그인 등록
 gsap.registerPlugin(ScrollTrigger);
 
@@ -76,45 +76,46 @@ const HomePage = (): JSX.Element => {
 
   return (
     <div>
-      {/* 콘텐츠 섹션 반복 렌더링 */}
+      
+          {/* 콘텐츠 섹션 반복 렌더링 */}
 
-      <ContentSection
-        title="프로젝트 추천"
-        description="기술과 경험을 분석한 최적의 프로젝트 추천"
-        color="rgb(251, 252, 248)"
-        DescriptionIcon={WorkExample}
-        WorkButton={DoneButton}
-      />
-      <ContentSection
-        title="성과 기반 점수"
-        description="성과 기반 점수로 서로의 신뢰도를 평가"
-        DescriptionIcon={EvaluationIcon}
-      />
-      <ContentSection
-        title="이상 계약 감지"
-        description="AI 기반 모델로 비정상적인 거래 감지"
-        color="rgb(251, 252, 248)"
-        DescriptionIcon={DetectionIcon}
-      />
-      <ContentSection
-        title="경력 증명서 발급"
-        description="NFT 경력 증명서로 안전한 경력 관리"
-        DescriptionIcon={RecommendIcon}
-      />
-      <ContentSection
-        title="스마트 계약 관리"
-        description="스마트 계약으로 프리랜서와 클라이언트의 안전한 계약 관리"
-        color="rgb(251, 252, 248)"
-        DescriptionIcon={ContractIcon}
-      />
+          <ContentSection
+            title="프로젝트 추천"
+            description="기술과 경험을 분석한 최적의 프로젝트 추천"
+            color="rgb(251, 252, 248)"
+            DescriptionIcon={WorkExample}
+            WorkButton={DoneButton}
+          />
+          <ContentSection
+            title="성과 기반 점수"
+            description="성과 기반 점수로 서로의 신뢰도를 평가"
+            DescriptionIcon={EvaluationIcon}
+          />
+          <ContentSection
+            title="이상 계약 감지"
+            description="AI 기반 모델로 비정상적인 거래 감지"
+            color="rgb(251, 252, 248)"
+            DescriptionIcon={DetectionIcon}
+          />
+          <ContentSection
+            title="경력 증명서 발급"
+            description="NFT 경력 증명서로 안전한 경력 관리"
+            DescriptionIcon={RecommendIcon}
+          />
+          <ContentSection
+            title="스마트 계약 관리"
+            description="스마트 계약으로 프리랜서와 클라이언트의 안전한 계약 관리"
+            color="rgb(251, 252, 248)"
+            DescriptionIcon={ContractIcon}
+          />
 
-      {/* 추가된 텍스트 효과 섹션 */}
-      <TextEffectSection />
-      <br />
-      <br />
-      <br />
-    </div>
-  );
+          {/* 추가된 텍스트 효과 섹션 */}
+          <TextEffectSection />
+          <br />
+          <br />
+          <br />
+        </div>
+        );
 };
 
-export default HomePage;
+        export default HomePage;

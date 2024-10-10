75f346fa13693792c4e63811cf912a90b52ff2d9
김연지 <yj009977@gmail.com>
2024-10-10 14:26:19 +0900

    fix : flask 수정

--- app.py
+++ app.py
@@ -32,34 +32,35 @@ def hi():
 
 @app.route('/flask/similar-boards', methods=['POST'])
 def find_similar_boards():
-    data = request.json
-    query_description = data['description']
+    return "hi"
+    # data = request.json
+    # query_description = data['description']
 
-    # Spring Boot 서버에서 모든 Board 데이터 가져오기
-    board_data = get_boards_from_spring()
+    # # Spring Boot 서버에서 모든 Board 데이터 가져오기
+    # board_data = get_boards_from_spring()
 
-    # print(board_data['data'])
+    # # print(board_data['data'])
 
-    # 모든 게시글의 description 임베딩
-    board_descriptions = [board['description'] for board in board_data['data']]
-    board_embeddings = model.encode(board_descriptions, convert_to_tensor=True)
+    # # 모든 게시글의 description 임베딩
+    # board_descriptions = [board['description'] for board in board_data['data']]
+    # board_embeddings = model.encode(board_descriptions, convert_to_tensor=True)
 
-    # 쿼리 description 임베딩
-    query_embedding = model.encode(query_description, convert_to_tensor=True)
+    # # 쿼리 description 임베딩
+    # query_embedding = model.encode(query_description, convert_to_tensor=True)
 
-    # 코사인 유사도 계산
-    cosine_scores = util.pytorch_cos_sim(query_embedding, board_embeddings).cpu().numpy()
+    # # 코사인 유사도 계산
+    # cosine_scores = util.pytorch_cos_sim(query_embedding, board_embeddings).cpu().numpy()
 
-    # # 유사도가 높은 순으로 정렬하여 상위 3개 게시글 선택
-    # similar_indices = cosine_scores.argsort()[0][::-1][:3]
-    # 유사도가 높은 순으로 정렬하여 상위 1개 게시글 선택
-    similar_indices = cosine_scores.argsort()[0][::-1][0] 
-    print(similar_indices)
-    # similar_boards = [board_data['data'][i] for i in similar_indices]
-    similar_board = board_data['data'][similar_indices]
-    print(similar_board)
+    # # # 유사도가 높은 순으로 정렬하여 상위 3개 게시글 선택
+    # # similar_indices = cosine_scores.argsort()[0][::-1][:3]
+    # # 유사도가 높은 순으로 정렬하여 상위 1개 게시글 선택
+    # similar_indices = cosine_scores.argsort()[0][::-1][0] 
+    # print(similar_indices)
+    # # similar_boards = [board_data['data'][i] for i in similar_indices]
+    # similar_board = board_data['data'][similar_indices]
+    # print(similar_board)
 
-    return jsonify(similar_board)
+    # return jsonify(similar_board)
 
 
 # Kafka 프로듀서 설정

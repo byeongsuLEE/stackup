2fa220f31a637a0f8776e3f6d1bfefb4d6c63c17
최승호 <148317140+S-Choi-1997@users.noreply.github.com>
2024-10-10 10:21:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import os
+import importlib
 from flask import Flask
 
 app = Flask(__name__)
+
+# ml 폴더 내의 모든 .py 파일을 동적으로 임포트
+ml_path = os.path.join(os.path.dirname(__file__), 'ml')
+for filename in os.listdir(ml_path):
+    if filename.endswith('.py'):
+        module_name = f"ml.{filename[:-3]}"  # .py 확장자 제거
+        importlib.import_module(module_name)
 
 @app.route('/flask')
 def hello():

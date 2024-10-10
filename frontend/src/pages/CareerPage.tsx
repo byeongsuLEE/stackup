3a38f43833c99e938cd9fb85bc57d61b9f90e85c
이호영 <hoo4053@naver.com>
2024-10-11 00:56:54 +0900

    nft test button

--- frontend/src/pages/CareerPage.tsx
+++ frontend/src/pages/CareerPage.tsx
@@ -2,9 +2,13 @@ import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import { Box, Tab } from "@mui/material";
-import React from "react";
-import NFTDisplay from "../components/NFTPage/NFTDisplay";
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
 import RegisteredCareerList from "../components/CareerPage/RegisteredCareerList";
+import NFTDisplay from "../components/NFTPage/NFTDisplay";
+import NFTMinting from "../components/NFTPage/NFTMinting";
+import { handlePrint } from "../hooks/MakePDF";
+import { CallTest } from "../hooks/Test";
 
 const Career = () => {
   const [value, setValue] = React.useState('1');
@@ -12,9 +16,40 @@ const Career = () => {
   const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
-  
+
+  const [pdf, setPdf] = useState<any>();
+  const { Minting, isLoading } = CallTest(); // Minting 함수 가져옴
+  const location = useLocation();
+  const projectId = location.state.projectId;
+
+
+  //== pdf 생성 ==//
+  const componentRef = useRef<HTMLDivElement>(null);
+
+
+
+  useEffect(() => {
+
+    //== pdf 생성 ==//
+    const MakePDF = async () => {
+      const pdfData = await handlePrint(componentRef);
+      setPdf(pdfData)
+    }
+
+    MakePDF();
+  }, [isLoading]);
+
+
+
+
   return (
     <div className='mt-10'>
+
+      <div ref={componentRef} >
+        아자자
+      </div>
+        <NFTMinting projectId={projectId} Minting={Minting} isLoading={isLoading} pdf={pdf}  />
+      
 
       <Box sx={{ width: '100%', typography: 'body1' }}>
         <TabContext value={value}>

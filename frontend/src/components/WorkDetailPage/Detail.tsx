927e04a04f687799d29148d4cc695276610d24d6
김연지 <yj009977@gmail.com>
2024-10-10 15:13:34 +0900

    fix : 디버그 확인

--- frontend/src/components/WorkDetailPage/Detail.tsx
+++ frontend/src/components/WorkDetailPage/Detail.tsx
@@ -88,7 +88,8 @@ const Detail = ({ project, clientId }: DetailProps) => {
     const checkAnomaly = async () => {
       try {
         const response = await axios.get(`${svURL}/api/board/detect/illegal/${project.boardId}`);
-        console.log(response.data)
+        console.log('이상거래 response : ',response)
+        console.log('이상거래 response.data : ',response.data)
         setIsAnomaly(response.data.is_anomaly[0]);
       } catch (error) {
         console.error("Error fetching anomaly data:", error);
